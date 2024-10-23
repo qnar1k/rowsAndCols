d32e95d7f6fbefbeb72edc167866f0085762a36a
Qnar <150032504+qnar1k@users.noreply.github.com>
2024-10-23 13:19:31 +0400

    Update rowsAndCols.cpp

--- rowsAndCols.cpp
+++ rowsAndCols.cpp
@@ -5,48 +5,50 @@ using namespace std;
 
 int main() {
 
-	char mat[10][10] = { 0 };
-	string text;
+    char mat[50][50] = { ' ' };  
+    string text;
 
-	cout << "Enter the text" << endl;
-	getline(cin, text);
-	cout << "Source data: " << text << endl;
-	
+    cout << "Enter the text" << endl;
+    getline(cin, text);
+    cout << "Source data: " << text << endl;
 
-	int size;
-	cout << "Enter the table size" << endl;
-	cin >> size;
+    int size;
+    cout << "Enter the table size" << endl;
+    cin >> size;
 
-	int k = 0, y = 0;
-	while (k != 12) {
-		for (int x = 0; x < size;x++) {
-			mat[y][x] = text[k++];
-		}
-		y++;
-	}
+    int k = 0, y = 0;
+    while (k < text.length()) {  issues
+        for (int x = 0; x < size; x++) {
+            if (k < text.length()) {
+                mat[y][x] = text[k++];
+            } else {
+                mat[y][x] = ' ';  
+            }
+        }
+        y++;
+    }
 
-	cout << endl;
+    cout << endl;
 
-	for (int i = 0; i < y; i++) {
-		for (int h = 0; h < size; h++){
-			cout << mat[i][h] << " ";
-		}
-		cout << endl;
-	}
+    for (int i = 0; i < y; i++) {
+        for (int h = 0; h < size; h++) {
+            cout << mat[i][h] << " ";
+        }
+        cout << endl;
+    }
 
-	ostringstream oss;
-	for (int i = 0; i < size; i++) {
-		for (int h = 0; h < y; h++) {
-			oss << mat[h][i];
-		}
-	}
+    ostringstream oss;
+    for (int i = 0; i < size; i++) {
+        for (int h = 0; h < y; h++) {
+            if (mat[h][i] != ' ') {   encryption
+                oss << mat[h][i];
+            }
+        }
+    }
 
-	string result = oss.str();
-	cout << "Encrypted string: " << endl;
-	cout << result;
+    string result = oss.str();
+    cout << "Encrypted string: " << endl;
+    cout << result;
 
-
-	return 0;
-
-		
+    return 0;
 }
