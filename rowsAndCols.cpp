010440d164c9f62b1c44d6313e8778861a770355
Qnar <150032504+qnar1k@users.noreply.github.com>
2024-10-30 10:22:56 +0400

    Update rowsAndCols.cpp

--- rowsAndCols.cpp
+++ rowsAndCols.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 
-    char mat[50][50] = { ' ' };  
+    char mat[50][50] = { ' ' };
     string text;
 
     cout << "Enter the text" << endl;
@@ -17,12 +17,13 @@ int main() {
     cin >> size;
 
     int k = 0, y = 0;
-    while (k < text.length()) {  
+    while (k < text.length()) {
         for (int x = 0; x < size; x++) {
             if (k < text.length()) {
                 mat[y][x] = text[k++];
-            } else {
-                mat[y][x] = ' ';  
+            }
+            else {
+                mat[y][x] = ' ';
             }
         }
         y++;
@@ -40,15 +41,24 @@ int main() {
     ostringstream oss;
     for (int i = 0; i < size; i++) {
         for (int h = 0; h < y; h++) {
-            if (mat[h][i] != ' ') {   
+            if (mat[h][i] != ' ') {
                 oss << mat[h][i];
             }
+        }
+    }
+    ostringstream oss1;
+    for (int i = 0; i < y; i++) {
+        for (int h = 0; h < size; h++) {
+             oss1 << mat[i][h]; 
         }
     }
 
     string result = oss.str();
     cout << "Encrypted string: " << endl;
-    cout << result;
+    cout << result << endl;
+    string result1 = oss1.str();
+    cout << "Decrypted string: " << endl;
+    cout << result1;
 
     return 0;
 }
